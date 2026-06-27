a40d64c76c2ee71b2ac023a2863d8b4bb40b87f0
prachisingh2804 <prachisingh2804vaibhav@gmail.com>
2026-06-27 18:05:39 +0530

    Add Student Grade Calculator project

---
+++ grade_calculator.py
@@ -0,0 +1,19 @@
+marks = int(input("Enter your marks (0-100): "))
+
+if marks < 0 or marks > 100:
+    print("Invalid marks!")
+
+elif marks >= 90:
+    print("Grade A")
+
+elif marks >= 75:
+    print("Grade B")
+
+elif marks >= 60:
+    print("Grade C")
+
+elif marks >= 40:
+    print("Grade D")
+
+else:
+    print("Fail")
